53a42d894d17b7352d5d9990cea60606e40cdef1
freudenthal <john.freudenthal@gmail.com>
2025-10-01 10:49:37 -0700

    Change to use a pointer to the SPIClass

--- AD5660.cpp
+++ AD5660.cpp
@@ -1,32 +1,35 @@
 #include "AD5660.h"
 
-AD5660::AD5660(byte CSSelectPin)
+AD5660::AD5660(byte CSSelectPin, SPIClass* spi)
+    : _spi(spi),
+      ConnectionSettings(1000000, MSBFIRST, SPI_MODE2),
+      PowerMode(PowerDownMode::Normal),
+      SelectPin(CSSelectPin)
 {
-	SelectPin = CSSelectPin;
-	PowerMode = PowerDownMode::Normal;
-	pinMode(CSSelectPin, OUTPUT);
-	digitalWrite(CSSelectPin, HIGH);
-	ConnectionSettings = SPISettings(1000000, MSBFIRST, SPI_MODE2);
+    pinMode(SelectPin, OUTPUT);
+    digitalWrite(SelectPin, HIGH);
 }
 
 void AD5660::Set(uint16_t Setting)
 {
-	SPI.beginTransaction(ConnectionSettings);
-	SPI.transfer( 0 );
-	digitalWrite(SelectPin,LOW);
-	SPI.transfer( static_cast<uint8_t>(PowerMode) );
-	SPI.transfer( (uint8_t)(Setting>>8) );
-	SPI.transfer( (uint8_t)(Setting & 0xFF) );
-	SPI.endTransaction();
-	digitalWrite(SelectPin, HIGH);
+    _spi->beginTransaction(ConnectionSettings);
+    digitalWrite(SelectPin, LOW);
+
+    // Frame: [CMD/PD bits][MSB][LSB]
+    _spi->transfer(static_cast<uint8_t>(PowerMode));
+    _spi->transfer(static_cast<uint8_t>(Setting >> 8));
+    _spi->transfer(static_cast<uint8_t>(Setting & 0xFF));
+
+    digitalWrite(SelectPin, HIGH);
+    _spi->endTransaction();
 }
 
 void AD5660::SetPowerDown(PowerDownMode Mode)
 {
-	PowerMode = Mode;
+    PowerMode = Mode;
 }
 
 SPISettings* AD5660::GetSPISettings()
 {
-	return &ConnectionSettings;
+    return &ConnectionSettings;
 }
--- AD5660.h
+++ AD5660.h
@@ -4,24 +4,30 @@
 #include <Arduino.h>
 #include <SPI.h>
 
-class AD5660
-{
-	public:
-		enum class PowerDownMode
-		{
-			Normal = 0,
-			k1ToGND = 1,
-			k100ToGND = 2,
-			TriState = 3,
-			Count = 4,
-		};
-		AD5660(byte CSSelectPin);
-		void Set(uint16_t Setting);
-		void SetPowerDown(PowerDownMode Mode);
-		SPISettings* GetSPISettings();
-	private:
-		SPISettings ConnectionSettings;
-		PowerDownMode PowerMode;
-		byte SelectPin;
+class AD5660 {
+public:
+    enum class PowerDownMode {
+        Normal    = 0,
+        k1ToGND   = 1,
+        k100ToGND = 2,
+        TriState  = 3,
+        Count     = 4,
+    };
+
+    // New: optionally pass which SPI bus to use (defaults to the global SPI)
+    AD5660(byte CSSelectPin, SPIClass* spi = &SPI);
+
+    void Set(uint16_t Setting);
+    void SetPowerDown(PowerDownMode Mode);
+
+    // Keep your existing accessor
+    SPISettings* GetSPISettings();
+
+private:
+    SPIClass*     _spi;               // pointer to the SPI interface to use
+    SPISettings   ConnectionSettings;
+    PowerDownMode PowerMode;
+    byte          SelectPin;
 };
+
 #endif
